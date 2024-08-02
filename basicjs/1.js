5b240d99d7c10974149b0370e094284baacbbda4
good5pal <152773762+good5pal@users.noreply.github.com>
2024-08-02 06:14:00 +0000

    add veriables

--- basicjs/1.js
+++ basicjs/1.js
@@ -1,5 +1,17 @@
 console.log("sonal patel")
+//  new file  //
+
 const accountid = 121212
-let accountemail = "abc@gmail.com"
-var accountpassword = "1345"
-console.log(accountid)
+let accountemail = "123@gmail.com"
+var accountpassword = "343444"
+acountcity = "bhopal"
+
+// we can change all but not change const //
+accountemail = "111@gmail.com"
+
+//  accountid = 2 // const only one time use
+console.log(accountemail) 
+
+// in table we show //
+
+console.table([accountid,accountemail,accountpassword,acountcity])
