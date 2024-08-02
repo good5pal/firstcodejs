1a04fa9105f1b823181916d95eec92d59b319612
good5pal <152773762+good5pal@users.noreply.github.com>
2024-08-02 07:03:15 +0000

    datatypes

---
+++ basicjs/2datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3+3) // we are using nodes, not browser
+
+console.log(3
+    +3)    // dont use like this
+
+console.log(3+3); console.log("abcd") //  dont write like this
+
+let name = "sonal"
+let age = 30
+let isloggin = false
+
+// number = 2 to power 53
+// bigint for longlike stockmarket
+// string ""
+// boolean = yes or no , true or flase
+// null use in standlun value
+// undefine = when we not define value
+// symbol = unique 
+
+// object
+
+console.log(typeof "sonu")
+console.log(typeof age)
+console.log(typeof null)  //null = object
+console.log(typeof undefined)
