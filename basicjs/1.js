eb5035a6f2b4bd8750eecba56785519ec60c8b3a
good5pal <152773762+good5pal@users.noreply.github.com>
2024-08-02 06:32:37 +0000

    add undefine var

--- basicjs/1.js
+++ basicjs/1.js
@@ -5,6 +5,7 @@ const accountid = 121212
 let accountemail = "123@gmail.com"
 var accountpassword = "343444"
 acountcity = "bhopal"
+let accountstate;
 
 // we can change all but not change const //
 accountemail = "111@gmail.com"
@@ -12,6 +13,13 @@ accountemail = "111@gmail.com"
 //  accountid = 2 // const only one time use
 console.log(accountemail) 
 
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+scop means {} 
+*/
+
 // in table we show //
 
-console.table([accountid,accountemail,accountpassword,acountcity])
+console.table([accountid,accountemail,accountpassword,acountcity,accountstate])
