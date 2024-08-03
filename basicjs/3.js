8a66f2389ba5dbbcce6b36e43a4e9084549bc82c
good5pal <152773762+good5pal@users.noreply.github.com>
2024-08-03 07:06:48 +0000

    add number sum

---
+++ basicjs/3.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+console.log("hello everyone");
+
+
+// add numbers
+
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2
+
+// disply the sum
+console.log("the sum is "  + sum);
